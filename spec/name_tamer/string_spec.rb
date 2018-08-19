f433d25980e0e4c68eb8e19592d69e6a08aa1426
Dominic Sayers <dominic@sayers.cc>
2018-08-19 12:22:30 +0100

    Update Rubocop config and auto-correct
    `bin/rubocop --auto-correct`

--- spec/name_tamer/string_spec.rb
+++ spec/name_tamer/string_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 describe String do
   it 'has a color' do
     expect('xxx'.yellow).to eq("\e[33mxxx\e[0m")
--- spec/name_tamer/text_spec.rb
+++ spec/name_tamer/text_spec.rb
@@ -1,4 +1,4 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
 describe NameTamer::Text do
   context '#segments' do
